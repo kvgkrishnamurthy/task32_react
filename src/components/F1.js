78a9cfc66f6989e46ffa7aa0764184fca07c0833
K V G Krishna Murthy <94794205+kvgkrishnamurthy@users.noreply.github.com>
2023-10-16 08:39:40 +0530

    Add files via upload

---
+++ src/components/F1.js
@@ -0,0 +1,71 @@
+import './style.css';
+function F1(){
+    return(
+        <div></div>
+    )
+}
+export function Home()
+{
+return <div class="vas"><p>my self kvg krishna murthy,i was born at Banglore.I comeleted my schooling in SRI CHAITHAINYA SCHOOL.Completed intermidiate fron sadhana juinor collage. Crrently pursuing my B-tech from vit ap university</p></div>
+}
+
+
+export function About()
+{
+return <div class="vas"><p>I am a Kind of Person Who is Interested In Exploring New Things and Learn something new every day. I like to Listen To POP Music And Watch Movies </p></div>
+}
+export function Blog()
+{
+    return <div class="vas">
+            <h2>Skills</h2>
+            <div className="skill-box">
+            <div class="d-flex flex-row justify-content-center">
+            <div className="language-box">
+             
+                <h5>Java</h5>
+            </div>
+            <div className="language-box">
+                
+                <h5>Python</h5>
+            </div>
+            <div className="language-box">
+               
+                <h5>HTML</h5>
+            </div>
+            </div><br/>
+            <div class="d-flex flex-row justify-content-center">
+            <div className="language-box">
+                
+                <h5>CSS</h5>
+            </div>
+            <div className="language-box">
+               
+                <h5>JavaScript</h5>
+            </div>
+            <div className="language-box">
+               
+                <h5>React.js</h5>
+            </div>
+            </div><br/>
+            <div class="d-flex flex-row justify-content-center">
+            <div className="language-box">
+                
+            </div>
+        
+            </div>
+            </div>
+        </div>
+}
+export function Contact()
+{
+return <div class='vas'>
+<div class="pran">
+    
+    
+     created my portfolio<br/> built using HTML,CSS and Javascript"
+</div>
+
+</div>
+}
+
+export default F1;
